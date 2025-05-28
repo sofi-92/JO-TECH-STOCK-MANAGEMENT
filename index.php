05938c2b48df2be074d26286fb57f22a9b7386ee
sofi-92 <sofibirhanu@gmail.com>
2025-05-28 18:05:04 +0300

    change title from GBC  to JO-TECH

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - JO TECH STOCK Management</title>
+    <title>Login - JO TECH STOCK MANAGEMENT </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
